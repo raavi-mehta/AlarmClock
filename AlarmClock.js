2c6b60124539b989a54f1c157f0f1f0aefd13b4a
mtarannu <mtarannu@ucalgary.ca>
2017-04-07 10:33:50 -0600

    bug fixed all the functions working so far

--- AlarmClock.js
+++ AlarmClock.js
@@ -34,7 +34,7 @@ function loadPastSession() {
             // converting  the date from jason format
             list[i].date = new Date(list[i].jsonDate);
             console.log("Converted tiime: " + list[i].date);
-
+            list[i].aId = i;
             var currentDate = new Date();
             console.log("current time: " + currentDate);
             if (list[i].jsonDate < currentDate.getTime()) {
